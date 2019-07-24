3ff843734c6fe1a223ab8fa88a1d078331bedf53
amha19 <amhatedla@gmail.com>
2019-07-24 20:25:36 +0200

    commit

---
+++ src/containers/BurgerBuilder/BurgerBuilder.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+import Auxiliary from '../../hoc/Auxiliary';
+
+
+class BurgerBuilder extends Component {
+    render() {
+        return (
+            <Auxiliary>
+                <div>Burger</div>
+                <div>Burger Control</div>
+            </Auxiliary>
+        );
+    }
+}
+
+export default BurgerBuilder;
